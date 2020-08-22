d35172f9cf5593fc33a5c473440de1e4126d2483
lucasart <lucas.braesch@gmail.com>
2020-08-22 19:17:28 +0800

    randon score in test engine
    Improves testing coverage a little.
    
    Integration test:
    $ ./c-chess-cli -cmd ./test/engine -name engine1:engine2 -sample 0.5 -depth 8 -openings test/chess960.epd -sample 0.5 -games 960 -pgnout out.pgn
    $ shasum out.pgn sample.bin
    9f2198c57a883d41485ce7392819666646aab717  out.pgn
    41d77eca60fd7943c1da25619d35dd4d7a010470  sample.bin

--- test/engine.c
+++ test/engine.c
@@ -93,7 +93,8 @@ static void run_go(const Position *pos, const Go *go, uint64_t *seed)
 
     for (int depth = 1; depth <= go->depth; depth++) {
         random_pv(pos, seed, depth, &pv);
-        uci_printf("info depth %d score cp %d pv %s\n", depth, 0, pv.buf);
+        uci_printf("info depth %d score cp %d pv %s\n", depth, (int)(prng(seed) % 65536) - 32768,
+            pv.buf);
     }
 
     scope(str_del) str_t token = {0};
