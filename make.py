88a318e0eeb88c326bc85653dba892fbdebf6e65
lucasart <lucas.braesch@gmail.com>
2020-10-19 09:24:34 +0800

    automate integration test and remove unit tests

--- make.py
+++ make.py
@@ -7,20 +7,8 @@ p.add_argument('-c', '--compiler', help='Compiler', choices=['cc', 'gcc', 'clang
 p.add_argument('-o', '--output', help='Output file', default='')
 p.add_argument('-d', '--debug', action='store_true', help='Debug compile')
 p.add_argument('-s', '--static', action='store_true', help='Static compile')
-p.add_argument('-p', '--program', help='Program to compile', choices=['main', 'test', 'engine'], default='main')
+p.add_argument('-p', '--task', help='Task to run', choices=['main', 'test', 'engine'], default='main')
 args = p.parse_args()
-
-# Select source files, and set output file, depending on which program is being compiled
-sources = 'src/bitboard.c src/gen.c src/position.c src/str.c src/util.c src/vec.c'
-if args.program == 'main':
-    sources += ' src/engine.c src/game.c src/main.c src/openings.c src/options.c src/sprt.c src/workers.c'
-    if args.output == '': args.output = './c-chess-cli'
-elif args.program == 'test':
-    sources += ' src/openings.c test/test.c'
-    if args.output == '': args.output = './test/test'
-elif args.program == 'engine':
-    sources += ' test/engine.c'
-    if args.output == '': args.output = './test/engine'
 
 # Determine flags for: compilation, warning, and linking
 cflags = '-I./src -std=gnu11 {} -mpopcnt -Ofast -flto'.format('-DNDEBUG -s' if not args.debug else '-g')
@@ -29,12 +17,40 @@ lflags ='-lpthread -lm'
 if args.static: lflags += ' -static'
 if 'ANDROID_DATA' in os.environ: lflags += ' -latomic'
 
-# Run compiler
-cmd = '{} {} {} {} -o {} {}'.format(args.compiler, cflags, wflags, sources, args.output, lflags)
-print(cmd)
-res = os.system(cmd)
+def run(cmd):
+    print('$ ' + cmd)
+    return os.system(cmd)
 
-# Run tests. Only if we compiled the test program, and compilation was successful
-if res == 0 and args.program == 'test':
-    print("running tests...")
-    os.system(args.output)
+def compile(program, output):
+    sources = 'src/bitboard.c src/gen.c src/position.c src/str.c src/util.c src/vec.c'
+    if program == 'main':
+        sources += ' src/engine.c src/game.c src/main.c src/openings.c src/options.c src/sprt.c' \
+            ' src/workers.c'
+    elif program == 'engine':
+        sources += ' test/engine.c'
+
+    return run('{} {} {} {} -o {} {}'.format(args.compiler, cflags, wflags, sources, output, lflags))
+
+if args.task == 'test':
+    if compile('engine', './test/engine') == 0 and compile('main', './c-chess-cli') == 0:
+        try:
+            os.remove('./out.pgn')
+            os.remove('./training.csv')
+            os.remove('./c-chess-cli.1.log')
+        except OSError:
+            pass
+
+        run('./c-chess-cli -each cmd=./test/engine depth=8 option.Hash=4 ' \
+            '-engine "name=engine\:1" option.Threads=2 -engine name=engine2 ' \
+            '-sample 0.5,y,training.csv -openings test/chess960.epd -games 965 ' \
+            '-log -pgnout out.pgn |shasum')
+        run('shasum out.pgn training.csv')
+        run('grep -v deadline c-chess-cli.1.log |shasum')
+
+elif args.task == 'main':
+    if args.output == '': args.output = './c-chess-cli'
+    compile(args.task, args.output)
+
+elif args.task == 'engine':
+    if args.output == '': args.output = './test/engine'
+    compile(args.task, args.output)
