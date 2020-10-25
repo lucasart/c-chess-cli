eb57d120a7604bc4931a87d6cbc5142ebe8cd478
lucasart <lucas.braesch@gmail.com>
2020-10-25 18:17:41 +0800

    Tournament: JobQueue

---
+++ src/jobs.c
@@ -0,0 +1,55 @@
+/*
+ * c-chess-cli, a command line interface for UCI chess engines. Copyright 2020 lucasart.
+ *
+ * c-chess-cli is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * c-chess-cli is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+*/
+#include "jobs.h"
+#include "vec.h"
+
+JobQueue job_queue_new(int engines, int rounds, int games)
+{
+    JobQueue jq = {0};
+    pthread_mutex_init(&jq.mtx, NULL);
+    jq.jobs = vec_new(Job);
+
+    // Gauntlet
+    assert(engines >= 2 && rounds >= 1 && games >= 1);
+
+    for (int r = 0; r < rounds; r++)
+        for (int e = 1; e < engines; e++)
+            for (int g = 0; g < games; g++) {
+                const Job j = {.e1 = 0, .e2 = e, .reverse = g % 2};
+                vec_push(jq.jobs, j);
+            }
+
+    return jq;
+}
+
+void job_queue_del(JobQueue *jq)
+{
+    vec_del(jq->jobs);
+    pthread_mutex_destroy(&jq->mtx);
+}
+
+bool job_queue_pop(JobQueue *jq, Job *j)
+{
+    pthread_mutex_lock(&jq->mtx);
+
+    const bool ok = vec_size(jq->jobs);
+
+    if (ok)
+        *j = vec_pop(jq->jobs);
+
+    pthread_mutex_unlock(&jq->mtx);
+
+    return ok;
+}
---
+++ src/jobs.h
@@ -0,0 +1,35 @@
+/*
+ * c-chess-cli, a command line interface for UCI chess engines. Copyright 2020 lucasart.
+ *
+ * c-chess-cli is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * c-chess-cli is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+*/
+#pragma once
+#include <pthread.h>
+#include <stdbool.h>
+
+// Job: instruction to play a single game
+typedef struct {
+    int e1, e2;  // engine[e1] plays engine[e2]
+    bool reverse;  // if true, e1 plays second
+    char pad[3];
+} Job;
+
+// Job Queue: consumed by workers to play tournament (thread safe)
+typedef struct {
+    pthread_mutex_t mtx;
+    Job *jobs;
+} JobQueue;
+
+JobQueue job_queue_new(int engines, int rounds, int games);
+void job_queue_del(JobQueue *jq);
+
+bool job_queue_pop(JobQueue *jq, Job *j);
