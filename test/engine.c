440ffb1c040aa3cbe74f7908be8bd8027baddaf0
lucasart <lucas.braesch@gmail.com>
2020-11-07 15:26:09 +0800

    cover full range of score in test-suite
    Including mate scores.
    
    $ ./make.py -p test
    % cc -I./src -std=gnu11 -mpopcnt -DNDEBUG -Ofast -flto -s -Wfatal-errors -Wall -Wextra -Wstrict-prototypes -Wsign-conversion -Wshadow -Wpadded src/bitboard.c src/gen.c src/position.c src/str.c src/util.c src/vec.c test/engine.c -o ./test/engine -lpthread -lm
    % cc -I./src -std=gnu11 -mpopcnt -DNDEBUG -Ofast -flto -s -Wfatal-errors -Wall -Wextra -Wstrict-prototypes -Wsign-conversion -Wshadow -Wpadded src/bitboard.c src/gen.c src/position.c src/str.c src/util.c src/vec.c src/engine.c src/game.c src/jobs.c src/main.c src/openings.c src/options.c src/sprt.c src/workers.c -o ./c-chess-cli -lpthread -lm
    % rm stdout* out*.pgn training.csv c-chess-cli.*.log log*
    
    Run tests:
    % ./c-chess-cli -each cmd=./test/engine depth=6 option.Hash=4 -engine name=engine=1 option.Threads=2 -engine name=engine2 depth=5 -sample 0.5,y,training.csv -openings test/chess960.epd -resign 5,900000000 -draw 3,700000000 -games 965 -log -pgn out1.pgn > stdout1
    % grep -v ^deadline c-chess-cli.1.log > log1
    % ./c-chess-cli -each "cmd=./test/engine 123" depth=3 -engine option.Hash=2 tc=10/0 -engine name=e2 tc=20/0 -engine name=e3 -openings test/chess960.epd -repeat -rounds 3 -games 50 -pgn out2.pgn -log > stdout2
    % grep -v ^deadline c-chess-cli.1.log > log2
    
    File signatures:
    % shasum stdout1 stdout2 out1.pgn out2.pgn log1 log2 training.csv
    ece51fd5e94646ddc4993ab61c243fc784e48dac  stdout1
    f6c7e0315b90ba98db2fd640496171e5b7b49823  stdout2
    977cce06287ad85a058972078a617117536f717b  out1.pgn
    7e18b60b1481e46ddae1e5e58070db71252a900d  out2.pgn
    20e1930f098a43014a206bb11f083815185af0e7  log1
    ad40f1b8bbd3e0bc56cf472661237374b65cf56f  log2
    c817323feb659220e09a77fbe9b0bd6f3c8c5a05  training.csv
    
    Overall signature:
    % cat stdout1 stdout2 out1.pgn out2.pgn log1 log2 training.csv |shasum
    4570741c2886c6aa64c6486b251768be3dd6d3ae  -

--- test/engine.c
+++ test/engine.c
@@ -90,7 +90,8 @@ static void run_go(const Position *pos, const Go *go, uint64_t *seed)
 
     for (int depth = 1; depth <= go->depth; depth++) {
         random_pv(pos, seed, depth, &pv);
-        uci_printf("info depth %d score cp %d pv %s\n", depth, (int)(prng(seed) % 65536) - 32768,
+        uci_printf("info depth %d score cp %d pv %s\n", depth,
+            (int)((prng(seed) & 0xFFFFFFFF) - 0x80000000),
             pv.buf);
     }
 
