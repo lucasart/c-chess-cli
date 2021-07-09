fd7f02aa8bd8c751635f645feaac702be44f268e
lucasart <lucas.braesch@gmail.com>
2021-07-09 19:45:34 +0800

    make.py -p clean

--- make.py
+++ make.py
@@ -7,7 +7,7 @@ p.add_argument('-c', '--compiler', help='Compiler', choices=['cc', 'gcc', 'clang
 p.add_argument('-o', '--output', help='Output file', default='')
 p.add_argument('-d', '--debug', action='store_true', help='Debug compile')
 p.add_argument('-s', '--static', action='store_true', help='Static compile')
-p.add_argument('-p', '--task', help='Task to run', choices=['main', 'test', 'engine'], default='main')
+p.add_argument('-p', '--task', help='Task to run', choices=['main', 'test', 'engine', 'clean'], default='main')
 args = p.parse_args()
 
 # Determine flags for: compilation, warning, and linking
@@ -35,10 +35,16 @@ def compile(program, output):
 
     return run('{} {} {} {} -o {} {}'.format(args.compiler, cflags, wflags, sources, output, lflags))
 
-if args.task == 'test':
-    if compile('engine', './test/engine') == 0 and compile('main', './c-chess-cli') == 0:
-        run('rm stdout* out*.pgn training.csv c-chess-cli.*.log log')
+def clean():
+    run('rm -f c-chess-cli test/engine stdout* out*.pgn training.csv c-chess-cli.*.log log')
 
+if args.task == 'clean':
+    clean()
+
+elif args.task == 'test':
+    clean()
+
+    if compile('engine', './test/engine') == 0 and compile('main', './c-chess-cli') == 0:
         print('\nRun tests:')
         run('./c-chess-cli -each cmd=./test/engine depth=6 option.Hash=4 ' \
             '-engine name=engine=1 option.Threads=2 -engine name=engine2 depth=5 ' \
